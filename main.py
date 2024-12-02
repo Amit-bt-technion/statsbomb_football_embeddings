20a62c9c303f83cb53d98196c09bfa8c6388eb2e
Ben-Tzvi, Amit <amit-ben@campus.technion.ac.il>
2024-12-02 08:12:19 +0200

    renamed 'embedder' module to 'tokenizer' to better represent functionality, and added basic main script

---
+++ main.py
@@ -0,0 +1,6 @@
+from tokenizer import Tokenizer
+
+if __name__ == "__main__":
+    tokenizer = Tokenizer(
+        "https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/events/15956.json")
+
