4e17bd18870b85cc4215e5c79ab82b691bc732fc
Ben-Tzvi, Amit <amit-ben@campus.technion.ac.il>
2024-12-04 09:23:15 +0200

    finished implementing get_value_of_nested_key util function

--- tokenizer/utils/helper_functions.py
+++ tokenizer/utils/helper_functions.py
@@ -1,5 +1,5 @@
 
-def get_value_of_nested_key(dictionary: dict, key: str, default=None):
+def get_value_of_nested_key(dictionary: dict, key: str, default=0):
     """
     this function iterates through a nested dictionary based on a dot ('.') notation of nested dictionary keys.
     :param dictionary: the dictionary in which to perform the lookup
@@ -7,8 +7,18 @@ def get_value_of_nested_key(dictionary: dict, key: str, default=None):
     :param default: what to return in case the desired key is not present in the dictionary
     :return: the value of the key if found in dictionary, and otherwise return the default value
     """
-    # TODO: attend list indexes as well
     for key in key.split('.'):
+        # if key contains [_], parsing as list index
+        if '[' in key:
+            key, index = key.split('[', 1)
+            index = index[:-1]
+            if key not in dictionary:
+                return default
+            li = dictionary[key]
+            if type(li) is not list or len(li) < index:
+                return default
+            dictionary = li[key]
+            continue
         if key not in dictionary:
             return default
         dictionary = dictionary[key]
