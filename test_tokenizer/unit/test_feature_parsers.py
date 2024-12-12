f499b5b4057b97335cdcafa842230a1b160e88d6
Amit-bt-technion <amit-ben@campus.technion.ac.il>
2024-12-12 23:10:15 +0200

    added unit tests for categoric parser

--- test_tokenizer/unit/test_feature_parsers.py
+++ test_tokenizer/unit/test_feature_parsers.py
@@ -1,4 +1,5 @@
 import unittest
+from parameterized import parameterized
 from tokenizer.feature_parsers import (
     CategoricalFeatureParser,
     RangeFeatureParser,
@@ -14,8 +15,16 @@ class TestTokenizer(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_categorical_feature_parser(self):
-        pass
+    @parameterized.expand([
+        ([0, 1], [0.5, 1]),
+        ([12, 58], [0.5, 1]),
+        ([809, 58], [1, 0.5]),
+        ([41, 1, 2, 100], [0.75, 0.25, 0.5, 1]),
+    ])
+    def test_categorical_feature_parser(self, categories, expected):
+        parser = CategoricalFeatureParser("test parser", categories)
+        for i, elem in enumerate(categories):
+            self.assertEquals(parser.get_normalized(elem), expected[i])
 
     def test_range_feature_parser(self):
         pass
