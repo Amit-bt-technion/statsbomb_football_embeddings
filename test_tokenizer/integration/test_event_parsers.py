c0ca00980782e0d3d13e254df7e61e1aff08d92e
Ben-Tzvi, Amit <amit-ben@campus.technion.ac.il>
2024-12-19 16:32:37 +0200

    fixed expected test results in test_event_parsers common sets for player position

--- test_tokenizer/integration/test_event_parsers.py
+++ test_tokenizer/integration/test_event_parsers.py
@@ -39,7 +39,7 @@ class TestEventParsers(unittest.TestCase):
             "possession_team": {"id": 222},
             "player": {"id": 32},
         }
-        expected_common_set_1 = [None, 1/3, 0.75, 0.25, 0.75, 0.5, 0.5, 1, 0.4, 0.2, 1, 0.5, 1, 0.5, 0.56]
+        expected_common_set_1 = [None, 1/3, 0.75, 0.25, 0.75, 0.5, 0.5, 1, 0.4, 0.2, 0.52, 0.5, 1, 0.5, 0.56]
 
         common_values_set_2 = {
             "type": {"id": -1},
@@ -57,7 +57,7 @@ class TestEventParsers(unittest.TestCase):
             "possession_team": {"id": 333},
             "player": {"id": 31},
         }
-        expected_common_set_2 = [None, 2/3, 0.8, 0.7, 0.375, 1, 1, 0.5, 0.8, 0.85, 1, 0.2, 1, 1, 0.28]
+        expected_common_set_2 = [None, 2/3, 0.8, 0.7, 0.375, 1, 1, 0.5, 0.8, 0.85, 0.84, 0.2, 1, 1, 0.28]
 
         # allowing iteration over all sets in all tests with minimal effort for adding new sets
         self.common_values_sets = [common_values_set_1, common_values_set_2]
@@ -91,10 +91,10 @@ class TestEventParsers(unittest.TestCase):
         # validating common values and specific-event values, and all other vector values are 0
         for cell_index in range(vector_size):
             if common_start_index <= cell_index < common_end_index:
-                self.assertEquals(vec[cell_index], expected_common[cell_index - common_start_index])
+                self.assertEqual(vec[cell_index], expected_common[cell_index - common_start_index])
             elif specific_start_index <= cell_index < specific_end_index:
-                self.assertEquals(vec[cell_index], expected[cell_index - specific_start_index])
+                self.assertEqual(vec[cell_index], expected[cell_index - specific_start_index])
             else:
                 a = vec[cell_index]
-                self.assertEquals(vec[cell_index], 0)
+                self.assertEqual(vec[cell_index], 0)
 
