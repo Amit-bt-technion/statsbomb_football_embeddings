21ec34239ce1f1c79db45122d3be7f0006d0a64c
Ben-Tzvi, Amit <amit-ben@campus.technion.ac.il>
2024-12-24 20:07:08 +0200

    added 4 full shot events from a match json

---
+++ test_tokenizer/integration/__init__.py
@@ -0,0 +1,803 @@
+
+# shot events taken from https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/events/16120.json
+
+shot_event_1 = {
+  "id" : "d3448b6e-b4eb-4d30-9e4e-ebbf95533d30",
+  "index" : 314,
+  "period" : 1,
+  "timestamp" : "00:05:09.168",
+  "minute" : 5,
+  "second" : 9,
+  "type" : {
+    "id" : 16,
+    "name" : "Shot"
+  },
+  "possession" : 10,
+  "possession_team" : {
+    "id" : 216,
+    "name" : "Getafe"
+  },
+  "play_pattern" : {
+    "id" : 1,
+    "name" : "Regular Play"
+  },
+  "team" : {
+    "id" : 216,
+    "name" : "Getafe"
+  },
+  "player" : {
+    "id" : 6611,
+    "name" : "Ángel Luis Rodríguez Díaz"
+  },
+  "position" : {
+    "id" : 22,
+    "name" : "Right Center Forward"
+  },
+  "location" : [ 103.7, 48.2 ],
+  "duration" : 0.808543,
+  "related_events" : [ "bb27d750-9f1a-401e-8c96-3e81423340d1" ],
+  "shot" : {
+    "statsbomb_xg" : 0.12042114,
+    "end_location" : [ 120.0, 40.8, 5.6 ],
+    "key_pass_id" : "d20d9571-7d6d-419d-b245-b2bc95f8eb23",
+    "technique" : {
+      "id" : 95,
+      "name" : "Volley"
+    },
+    "first_time" : True,
+    "outcome" : {
+      "id" : 98,
+      "name" : "Off T"
+    },
+    "body_part" : {
+      "id" : 40,
+      "name" : "Right Foot"
+    },
+    "type" : {
+      "id" : 87,
+      "name" : "Open Play"
+    },
+    "freeze_frame" : [ {
+      "location" : [ 105.1, 39.8 ],
+      "player" : {
+        "id" : 6379,
+        "name" : "Sergi Roberto Carnicer"
+      },
+      "position" : {
+        "id" : 2,
+        "name" : "Right Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 91.4, 32.6 ],
+      "player" : {
+        "id" : 6873,
+        "name" : "Francisco Portillo Soler"
+      },
+      "position" : {
+        "id" : 16,
+        "name" : "Left Midfield"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 108.8, 51.4 ],
+      "player" : {
+        "id" : 6826,
+        "name" : "Clément Lenglet"
+      },
+      "position" : {
+        "id" : 5,
+        "name" : "Left Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 118.4, 42.7 ],
+      "player" : {
+        "id" : 20055,
+        "name" : "Marc-André ter Stegen"
+      },
+      "position" : {
+        "id" : 1,
+        "name" : "Goalkeeper"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 113.7, 63.4 ],
+      "player" : {
+        "id" : 5213,
+        "name" : "Gerard Piqué Bernabéu"
+      },
+      "position" : {
+        "id" : 3,
+        "name" : "Right Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 103.5, 53.1 ],
+      "player" : {
+        "id" : 5470,
+        "name" : "Ivan Rakitić"
+      },
+      "position" : {
+        "id" : 10,
+        "name" : "Center Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 101.1, 72.2 ],
+      "player" : {
+        "id" : 5211,
+        "name" : "Jordi Alba Ramos"
+      },
+      "position" : {
+        "id" : 6,
+        "name" : "Left Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 96.4, 65.6 ],
+      "player" : {
+        "id" : 4546,
+        "name" : "Dimitri Foulquier"
+      },
+      "position" : {
+        "id" : 12,
+        "name" : "Right Midfield"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 114.2, 72.4 ],
+      "player" : {
+        "id" : 11550,
+        "name" : "Jaime Mata Arnaiz"
+      },
+      "position" : {
+        "id" : 24,
+        "name" : "Left Center Forward"
+      },
+      "teammate" : True
+    } ]
+  }
+}
+
+
+shot_event_2 = {
+  "id" : "eb50d981-f261-4c2e-ba44-879ce1906209",
+  "index" : 3329,
+  "period" : 2,
+  "timestamp" : "00:37:17.976",
+  "minute" : 82,
+  "second" : 17,
+  "type" : {
+    "id" : 16,
+    "name" : "Shot"
+  },
+  "possession" : 131,
+  "possession_team" : {
+    "id" : 217,
+    "name" : "Barcelona"
+  },
+  "play_pattern" : {
+    "id" : 1,
+    "name" : "Regular Play"
+  },
+  "team" : {
+    "id" : 217,
+    "name" : "Barcelona"
+  },
+  "player" : {
+    "id" : 5503,
+    "name" : "Lionel Andrés Messi Cuccittini"
+  },
+  "position" : {
+    "id" : 17,
+    "name" : "Right Wing"
+  },
+  "location" : [ 92.9, 40.1 ],
+  "duration" : 1.047749,
+  "under_pressure" : True,
+  "related_events" : [ "527748f1-43af-4328-bff8-0f50f3b3382a", "f2c15f0f-d7e5-4619-bcc5-000f8d48662b" ],
+  "shot" : {
+    "statsbomb_xg" : 0.021718958,
+    "end_location" : [ 117.1, 38.1, 0.7 ],
+    "key_pass_id" : "c31bd61b-47be-46c5-b271-73246a91801a",
+    "type" : {
+      "id" : 87,
+      "name" : "Open Play"
+    },
+    "outcome" : {
+      "id" : 100,
+      "name" : "Saved"
+    },
+    "technique" : {
+      "id" : 93,
+      "name" : "Normal"
+    },
+    "body_part" : {
+      "id" : 38,
+      "name" : "Left Foot"
+    },
+    "freeze_frame" : [ {
+      "location" : [ 116.3, 41.6 ],
+      "player" : {
+        "id" : 6722,
+        "name" : "David Soria Solís"
+      },
+      "position" : {
+        "id" : 1,
+        "name" : "Goalkeeper"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 100.6, 50.6 ],
+      "player" : {
+        "id" : 6619,
+        "name" : "Vitorino Gabriel Pacheco Antunes"
+      },
+      "position" : {
+        "id" : 6,
+        "name" : "Left Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 100.3, 46.1 ],
+      "player" : {
+        "id" : 6635,
+        "name" : "Leandro Daniel Cabrera Sasía"
+      },
+      "position" : {
+        "id" : 5,
+        "name" : "Left Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 86.3, 20.4 ],
+      "player" : {
+        "id" : 5470,
+        "name" : "Ivan Rakitić"
+      },
+      "position" : {
+        "id" : 13,
+        "name" : "Right Center Midfield"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 97.7, 16.3 ],
+      "player" : {
+        "id" : 5211,
+        "name" : "Jordi Alba Ramos"
+      },
+      "position" : {
+        "id" : 6,
+        "name" : "Left Back"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 84.3, 33.9 ],
+      "player" : {
+        "id" : 11392,
+        "name" : "Arthur Henrique Ramos de Oliveira Melo"
+      },
+      "position" : {
+        "id" : 15,
+        "name" : "Left Center Midfield"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 91.3, 67.1 ],
+      "player" : {
+        "id" : 6379,
+        "name" : "Sergi Roberto Carnicer"
+      },
+      "position" : {
+        "id" : 2,
+        "name" : "Right Back"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 99.5, 47.0 ],
+      "player" : {
+        "id" : 5246,
+        "name" : "Luis Alberto Suárez Díaz"
+      },
+      "position" : {
+        "id" : 23,
+        "name" : "Center Forward"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 100.4, 26.5 ],
+      "player" : {
+        "id" : 5477,
+        "name" : "Ousmane Dembélé"
+      },
+      "position" : {
+        "id" : 21,
+        "name" : "Left Wing"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 101.5, 22.5 ],
+      "player" : {
+        "id" : 6873,
+        "name" : "Francisco Portillo Soler"
+      },
+      "position" : {
+        "id" : 12,
+        "name" : "Right Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 101.8, 37.8 ],
+      "player" : {
+        "id" : 6634,
+        "name" : "Djené Dakonam Ortega"
+      },
+      "position" : {
+        "id" : 3,
+        "name" : "Right Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 102.1, 29.8 ],
+      "player" : {
+        "id" : 6627,
+        "name" : "Damián Nicolás Suárez Suárez"
+      },
+      "position" : {
+        "id" : 2,
+        "name" : "Right Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 95.0, 37.9 ],
+      "player" : {
+        "id" : 6863,
+        "name" : "Mauro Wilney Arambarri Rosa"
+      },
+      "position" : {
+        "id" : 11,
+        "name" : "Left Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 83.5, 24.8 ],
+      "player" : {
+        "id" : 11550,
+        "name" : "Jaime Mata Arnaiz"
+      },
+      "position" : {
+        "id" : 22,
+        "name" : "Right Center Forward"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 95.6, 40.6 ],
+      "player" : {
+        "id" : 6901,
+        "name" : "Nemanja Maksimović"
+      },
+      "position" : {
+        "id" : 9,
+        "name" : "Right Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 93.8, 50.7 ],
+      "player" : {
+        "id" : 4712,
+        "name" : "Samuel Sáiz Alonso"
+      },
+      "position" : {
+        "id" : 16,
+        "name" : "Left Midfield"
+      },
+      "teammate" : False
+    } ]
+  }
+}
+
+shot_event_3 = {
+  "id" : "9b2f6108-6b19-4756-92ff-3893306df5e4",
+  "index" : 2244,
+  "period" : 2,
+  "timestamp" : "00:08:12.329",
+  "minute" : 53,
+  "second" : 12,
+  "type" : {
+    "id" : 16,
+    "name" : "Shot"
+  },
+  "possession" : 73,
+  "possession_team" : {
+    "id" : 217,
+    "name" : "Barcelona"
+  },
+  "play_pattern" : {
+    "id" : 3,
+    "name" : "From Free Kick"
+  },
+  "team" : {
+    "id" : 217,
+    "name" : "Barcelona"
+  },
+  "player" : {
+    "id" : 5503,
+    "name" : "Lionel Andrés Messi Cuccittini"
+  },
+  "position" : {
+    "id" : 17,
+    "name" : "Right Wing"
+  },
+  "location" : [ 106.1, 31.1 ],
+  "duration" : 0.609344,
+  "related_events" : [ "1444801b-9aa7-468c-9004-60cb324d1dd8" ],
+  "shot" : {
+    "statsbomb_xg" : 0.105519824,
+    "end_location" : [ 119.3, 37.8, 1.7 ],
+    "type" : {
+      "id" : 87,
+      "name" : "Open Play"
+    },
+    "outcome" : {
+      "id" : 100,
+      "name" : "Saved"
+    },
+    "first_time" : True,
+    "technique" : {
+      "id" : 91,
+      "name" : "Half Volley"
+    },
+    "body_part" : {
+      "id" : 40,
+      "name" : "Right Foot"
+    },
+    "freeze_frame" : [ {
+      "location" : [ 99.3, 29.9 ],
+      "player" : {
+        "id" : 6901,
+        "name" : "Nemanja Maksimović"
+      },
+      "position" : {
+        "id" : 9,
+        "name" : "Right Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 113.4, 26.7 ],
+      "player" : {
+        "id" : 6634,
+        "name" : "Djené Dakonam Ortega"
+      },
+      "position" : {
+        "id" : 3,
+        "name" : "Right Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 110.2, 24.4 ],
+      "player" : {
+        "id" : 6627,
+        "name" : "Damián Nicolás Suárez Suárez"
+      },
+      "position" : {
+        "id" : 2,
+        "name" : "Right Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 111.4, 37.6 ],
+      "player" : {
+        "id" : 6635,
+        "name" : "Leandro Daniel Cabrera Sasía"
+      },
+      "position" : {
+        "id" : 5,
+        "name" : "Left Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 109.1, 44.3 ],
+      "player" : {
+        "id" : 6619,
+        "name" : "Vitorino Gabriel Pacheco Antunes"
+      },
+      "position" : {
+        "id" : 6,
+        "name" : "Left Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 105.8, 32.4 ],
+      "player" : {
+        "id" : 6863,
+        "name" : "Mauro Wilney Arambarri Rosa"
+      },
+      "position" : {
+        "id" : 11,
+        "name" : "Left Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 99.7, 47.7 ],
+      "player" : {
+        "id" : 8206,
+        "name" : "Arturo Erasmo Vidal Pardo"
+      },
+      "position" : {
+        "id" : 13,
+        "name" : "Right Center Midfield"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 111.4, 50.1 ],
+      "player" : {
+        "id" : 5246,
+        "name" : "Luis Alberto Suárez Díaz"
+      },
+      "position" : {
+        "id" : 23,
+        "name" : "Center Forward"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 119.5, 38.2 ],
+      "player" : {
+        "id" : 6722,
+        "name" : "David Soria Solís"
+      },
+      "position" : {
+        "id" : 1,
+        "name" : "Goalkeeper"
+      },
+      "teammate" : False
+    } ]
+  }
+}
+
+shot_event_4 = {
+  "id" : "f96397fc-0e5d-4d04-884c-88074b4728a3",
+  "index" : 1076,
+  "period" : 1,
+  "timestamp" : "00:25:30.615",
+  "minute" : 25,
+  "second" : 30,
+  "type" : {
+    "id" : 16,
+    "name" : "Shot"
+  },
+  "possession" : 33,
+  "possession_team" : {
+    "id" : 217,
+    "name" : "Barcelona"
+  },
+  "play_pattern" : {
+    "id" : 2,
+    "name" : "From Corner"
+  },
+  "team" : {
+    "id" : 217,
+    "name" : "Barcelona"
+  },
+  "player" : {
+    "id" : 5213,
+    "name" : "Gerard Piqué Bernabéu"
+  },
+  "position" : {
+    "id" : 3,
+    "name" : "Right Center Back"
+  },
+  "location" : [ 114.4, 35.3 ],
+  "duration" : 0.432287,
+  "related_events" : [ "2db87634-de97-471f-9807-59ca11e73eab" ],
+  "shot" : {
+    "statsbomb_xg" : 0.37003344,
+    "end_location" : [ 119.0, 40.0, 0.7 ],
+    "key_pass_id" : "f56422f9-d711-4e28-a566-ca57fa7a453f",
+    "type" : {
+      "id" : 87,
+      "name" : "Open Play"
+    },
+    "outcome" : {
+      "id" : 100,
+      "name" : "Saved"
+    },
+    "first_time" : True,
+    "technique" : {
+      "id" : 93,
+      "name" : "Normal"
+    },
+    "body_part" : {
+      "id" : 38,
+      "name" : "Left Foot"
+    },
+    "freeze_frame" : [ {
+      "location" : [ 102.6, 27.3 ],
+      "player" : {
+        "id" : 11550,
+        "name" : "Jaime Mata Arnaiz"
+      },
+      "position" : {
+        "id" : 24,
+        "name" : "Left Center Forward"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 109.1, 33.6 ],
+      "player" : {
+        "id" : 6863,
+        "name" : "Mauro Wilney Arambarri Rosa"
+      },
+      "position" : {
+        "id" : 11,
+        "name" : "Left Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 111.0, 34.6 ],
+      "player" : {
+        "id" : 5477,
+        "name" : "Ousmane Dembélé"
+      },
+      "position" : {
+        "id" : 21,
+        "name" : "Left Wing"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 110.0, 40.1 ],
+      "player" : {
+        "id" : 6619,
+        "name" : "Vitorino Gabriel Pacheco Antunes"
+      },
+      "position" : {
+        "id" : 6,
+        "name" : "Left Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 117.0, 32.7 ],
+      "player" : {
+        "id" : 6611,
+        "name" : "Ángel Luis Rodríguez Díaz"
+      },
+      "position" : {
+        "id" : 22,
+        "name" : "Right Center Forward"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 114.4, 45.6 ],
+      "player" : {
+        "id" : 6634,
+        "name" : "Djené Dakonam Ortega"
+      },
+      "position" : {
+        "id" : 3,
+        "name" : "Right Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 114.5, 42.6 ],
+      "player" : {
+        "id" : 6627,
+        "name" : "Damián Nicolás Suárez Suárez"
+      },
+      "position" : {
+        "id" : 2,
+        "name" : "Right Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 109.8, 53.1 ],
+      "player" : {
+        "id" : 8206,
+        "name" : "Arturo Erasmo Vidal Pardo"
+      },
+      "position" : {
+        "id" : 13,
+        "name" : "Right Center Midfield"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 110.5, 42.9 ],
+      "player" : {
+        "id" : 6826,
+        "name" : "Clément Lenglet"
+      },
+      "position" : {
+        "id" : 5,
+        "name" : "Left Center Back"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 111.0, 47.1 ],
+      "player" : {
+        "id" : 5246,
+        "name" : "Luis Alberto Suárez Díaz"
+      },
+      "position" : {
+        "id" : 23,
+        "name" : "Center Forward"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 107.3, 27.2 ],
+      "player" : {
+        "id" : 6873,
+        "name" : "Francisco Portillo Soler"
+      },
+      "position" : {
+        "id" : 16,
+        "name" : "Left Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 113.2, 30.4 ],
+      "player" : {
+        "id" : 6901,
+        "name" : "Nemanja Maksimović"
+      },
+      "position" : {
+        "id" : 9,
+        "name" : "Right Defensive Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 115.2, 32.6 ],
+      "player" : {
+        "id" : 6635,
+        "name" : "Leandro Daniel Cabrera Sasía"
+      },
+      "position" : {
+        "id" : 5,
+        "name" : "Left Center Back"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 114.8, 26.3 ],
+      "player" : {
+        "id" : 5503,
+        "name" : "Lionel Andrés Messi Cuccittini"
+      },
+      "position" : {
+        "id" : 17,
+        "name" : "Right Wing"
+      },
+      "teammate" : True
+    }, {
+      "location" : [ 102.5, 24.3 ],
+      "player" : {
+        "id" : 4546,
+        "name" : "Dimitri Foulquier"
+      },
+      "position" : {
+        "id" : 12,
+        "name" : "Right Midfield"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 119.0, 37.6 ],
+      "player" : {
+        "id" : 6722,
+        "name" : "David Soria Solís"
+      },
+      "position" : {
+        "id" : 1,
+        "name" : "Goalkeeper"
+      },
+      "teammate" : False
+    }, {
+      "location" : [ 119.4, 26.9 ],
+      "player" : {
+        "id" : 11392,
+        "name" : "Arthur Henrique Ramos de Oliveira Melo"
+      },
+      "position" : {
+        "id" : 15,
+        "name" : "Left Center Midfield"
+      },
+      "teammate" : True
+    } ]
+  }
+}
