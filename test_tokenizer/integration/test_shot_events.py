789eb9d681bce7b1626b0843be88f0f22ef64030
Ben-Tzvi, Amit <amit-ben@campus.technion.ac.il>
2024-12-26 15:53:42 +0200

    implemented shot event integration test function

---
+++ test_tokenizer/integration/test_shot_events.py
@@ -0,0 +1,23 @@
+import unittest
+from parameterized import parameterized
+from tokenizer.match_parser import MatchEventsParser
+from tokenizer import vector_size
+from test_tokenizer.integration import (
+    shot_event_1, shot_event_2, shot_event_3, shot_event_4,
+    teams_and_players_1, teams_and_players_2, teams_and_players_3, teams_and_players_4,
+    expected_shot_event_1, expected_shot_event_2, expected_shot_event_3, expected_shot_event_4
+)
+
+parser = MatchEventsParser(vector_size)
+
+
+class TestShotEvents(unittest.TestCase):
+    @parameterized.expand([
+        (shot_event_1, teams_and_players_1, expected_shot_event_1),
+        (shot_event_2, teams_and_players_2, expected_shot_event_2),
+        (shot_event_3, teams_and_players_3, expected_shot_event_3),
+        (shot_event_4, teams_and_players_4, expected_shot_event_4),
+    ])
+    def test_shot_events(self, event, tnp, expected):
+        parser.teams_and_players = tnp
+        self.assertEqual(parser.parse_event(event), expected)
